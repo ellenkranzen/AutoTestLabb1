3d6b40bc399b743189fc124e60ae13977c1aa2fe
Ellen Kranzen <ellen.kranzen@gmail.com>
2021-02-15 15:32:28 +0100

    Gjort tester för VG-delen - Ellen

--- Test/TestEmployeeSystem.java
+++ Test/TestEmployeeSystem.java
@@ -30,4 +30,36 @@ public class TestEmployeeSystem {
         testObject.deleteEmployee(0);
         Assertions.assertTrue(testObject.getList().size() == 0);
     }
+
+    @Test
+    //TODO lägg till DisplayName
+    public void testRaiseIndSalary() {
+        EmployeeSystem testSys = new EmployeeSystem();
+        testSys.addEmployee(new Employee("Jonna Hagberg", 34, 50000.00));
+        double expected = 55000.00;
+        testSys.raiseIndSalary(0, 0.10);
+        double actual = testSys.get(0).getSalary();
+        Assertions.assertEquals(expected, actual);
+        testSys.raiseIndSalary(0, 1.10);  //testar att höja med mer än 100%
+        Assertions.assertEquals(expected, actual);    // säkerställer att lönen inte har ändrats
+    }
+
+    @Test
+    //TODO lägg till DispLayName
+    public void testRaiseAllSalary() {
+        EmployeeSystem testSys = new EmployeeSystem();
+        testSys.addEmployee(new Employee("Jonna Hagberg", 34, 50000.00));
+        testSys.addEmployee(new Employee("Ellen Kranzén", 29, 45000.00));
+        testSys.raiseAllSalary(0.05);
+        double expectedEmp1 = 52500.00;
+        double expectedEmp2 = 47250.00;
+        double actualEmp1 = testSys.get(0).getSalary();
+        double actualEmp2 = testSys.get(1).getSalary();
+        Assertions.assertEquals(expectedEmp1, actualEmp1);
+        Assertions.assertEquals(expectedEmp2, actualEmp2);
+        testSys.raiseAllSalary(1.05);                       //testar att försöka höja med mer än 100%
+        Assertions.assertEquals(expectedEmp1, actualEmp1);  //säkerställer att lönen inte har ändrats
+        Assertions.assertEquals(expectedEmp2, actualEmp2);  // same
+    }
+
 }
--- src/main/java/EmployeeSystem.java
+++ src/main/java/EmployeeSystem.java
@@ -14,4 +14,23 @@ public class EmployeeSystem {
      public void deleteEmployee(int place) {
           employeeList.remove(place);
      }
+
+     public void raiseIndSalary(int index, double raise) {
+          if(raise > 0 && raise < 100) {
+               Employee emp = employeeList.get(index);
+               double currentSal = emp.getSalary();
+               double newSal = currentSal + currentSal*raise;
+               emp.setSalary(newSal);
+          }
+     }
+
+     public Employee get(int index) {
+          return employeeList.get(index);
+     }
+
+     public void raiseAllSalary(double raise) {
+          for(int i = 0; i < employeeList.size(); i++) {
+               raiseIndSalary(i, raise);
+          }
+     }
 }
